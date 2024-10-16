135f13614a6640e6f1d34b46249b097b399135da
Islam Mac <islam@mac.com>
2024-10-16 20:10:59 +0300

    mvp

---
+++ database/migrations/2024_10_16_160805_create_course_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Course;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Course::class)->constrained();
+            $table->foreignIdFor(User::class)->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_user');
+    }
+};
